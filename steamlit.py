701ec4df00e90778ff2a07cee431fca7b9730626
NatdanaiDan <firstfay92@gmail.com>
2024-01-03 09:38:45 +0700

    update streamltit

--- steamlit.py
+++ steamlit.py
@@ -3,9 +3,18 @@ import pandas as pd
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-pd.set_option("display.max_colwidth", 0)
+# pd.set_option("display.max_colwidth", 0)
 
+# Your MongoDB URI
 uri = "mongodb+srv://64015037:2YqYA4kjsTImOMmY@keytoad.nslb9f9.mongodb.net/?retryWrites=true&w=majority"
+js = """
+<script>
+    var body = window.parent.document.querySelector(".main");
+    console.log(body);
+    body.scrollTop = 0;
+</script>
+"""
+
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi("1"))
@@ -21,10 +30,12 @@ st.set_page_config(
 
 st.title("KeyToAd")
 
+# Your MongoDB setup
 db = client["keytoad"]
 collection = db["keytoad"]
 emo_collection = db["emodata"]
 
+# Your Streamlit session state setup
 if "state" not in st.session_state:
     st.session_state["state"] = False
 
@@ -52,37 +63,64 @@ def getrandomdata():
 
 
 def sendapi(data):
-    st.info(f"Sending API... {data}")
-
+    # st.info(f"Sending API... {data}")
+    # print(data)
     # Insert data into emo_collection
-    emo_collection.insert_many(data)
+    # with st.spinner("Wait for it..."):
+    #     emo_collection.insert_many(data)
 
-    st.success("Send API Success")
     st.session_state["state"] = False
     st.session_state["data"] = None
+    st.success("Send API Success")
+    st.components.v1.html(js)
+    # st.balloons(s)
 
 
+class StreamlitDataEditor:
+    def __init__(self, data, options):
+        self.data = data
+        self.options = options
+
+    def generate_editor(self):
+        for i in range(len(self.data)):
+            st.write(f"keyword: {self.data[i]['context']}")
+            st.write(f"Generate: {self.data[i]['bot']}")
+            self.data[i]["Emotion"] = st.selectbox(
+                f"Emotion for item {i+1}",
+                options=self.options,
+                key=f"emotion_{i}",
+            )
+
+    def is_data_available(self):
+        return self.data is not None
+
+
+# Usage
 if st.button("Get Random Data"):
     st.session_state["state"] = True
-    data = getrandomdata()
-    st.session_state["data"] = data
+    with st.status("Loading Data..."):
+        st.session_state["data"] = getrandomdata()
 
-edited_df = st.data_editor(
+data_editor = StreamlitDataEditor(
     data=st.session_state["data"] if st.session_state["state"] else None,
-    column_config={
-        "Emotion": st.column_config.SelectboxColumn(
-            "Emotion",
-            options=["Normal", "Luxury", "Chill", "Exclusive", "Bad"],
-            required=True,
-        )
-    },
-    # width=3000,
-    use_container_width=True,
+    options=["Normal", "Luxury", "Chill", "Exclusive", "Bad"],
 )
 
-st.table(edited_df)
+if data_editor.is_data_available():
+    data_editor.generate_editor()
+
 if st.button("Send API"):
     if st.session_state["state"]:
-        sendapi(edited_df)
+        sendapi(st.session_state["data"])
     else:
         st.warning("Please get random data first")
+
+# if st.button("Send API and Get Random Data"):
+#     if st.session_state["state"]:
+#         sendapi(st.session_state["data"])
+#         st.session_state["state"] = True
+#         with st.status("Loading Data..."):
+#             st.session_state["data"] = getrandomdata()
+#             print(st.session_state["data"])
+#     else:
+#         st.warning("Please get random data first")
