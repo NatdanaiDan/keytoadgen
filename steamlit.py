ca49d131653b375755abc1db802dd9110e832cf0
NatdanaiDan <firstfay92@gmail.com>
2024-01-03 09:57:21 +0700

    update streamltit

--- steamlit.py
+++ steamlit.py
@@ -1,79 +1,88 @@
 import streamlit as st
-import pandas as pd
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-# pd.set_option("display.max_colwidth", 0)
 
-# Your MongoDB URI
-uri = "mongodb+srv://64015037:2YqYA4kjsTImOMmY@keytoad.nslb9f9.mongodb.net/?retryWrites=true&w=majority"
-js = """
-<script>
-    var body = window.parent.document.querySelector(".main");
-    console.log(body);
-    body.scrollTop = 0;
-</script>
-"""
+class KeyToAdApp:
+    def __init__(self, mongo_uri):
+        self.client = MongoClient(mongo_uri, server_api=ServerApi("1"))
+        self.db = self.client["keytoad"]
+        self.collection = self.db["keytoad"]
+        self.emo_collection = self.db["emodata"]
 
+        self.options = ["Normal", "Luxury", "Chill", "Exclusive", "Bad"]
+        self.data_editor = None
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi("1"))
+        st.set_page_config(
+            page_title="KeyToAd",
+            page_icon="📣",
+            layout="wide",
+            initial_sidebar_state="expanded",
+        )
 
-from bson.objectid import ObjectId
+        if "state" not in st.session_state:
+            st.session_state["state"] = False
 
-st.set_page_config(
-    page_title="KeyToAd",
-    page_icon="📣",
-    layout="wide",
-    initial_sidebar_state="expanded",
-)
+        if "data" not in st.session_state:
+            st.session_state["data"] = None
 
-st.title("KeyToAd")
+    def get_random_data(self):
+        listdata = list(self.collection.aggregate([{"$sample": {"size": 5}}]))
 
-# Your MongoDB setup
-db = client["keytoad"]
-collection = db["keytoad"]
-emo_collection = db["emodata"]
+        status = True
+        while status:
+            for item in listdata:
+                if self.emo_collection.find_one({"_id": item["_id"]}):
+                    listdata = list(
+                        self.collection.aggregate([{"$sample": {"size": 5}}])
+                    )
+                    break
+            else:
+                status = False
 
-# Your Streamlit session state setup
-if "state" not in st.session_state:
-    st.session_state["state"] = False
+        return listdata
 
-if "data" not in st.session_state:
-    st.session_state["data"] = None
+    def send_api(self, data):
+        with st.spinner("Wait for it..."):
+            self.emo_collection.insert_many(data)
+        # st.info(data)
 
+        st.session_state["state"] = False
+        st.session_state["data"] = None
+        st.success("Send API Success")
 
-def getrandomdata():
-    listdata = list(collection.aggregate([{"$sample": {"size": 5}}]))
-    # print objectid in listdata
-    status = True
+    def run(self):
+        st.title("KeyToAd")
 
-    while status:
-        for item in listdata:
-            # Check if _id already exists in emo_collection
-            if emo_collection.find_one({"_id": item["_id"]}):
-                # If duplicate _id, get new random data
-                listdata = list(collection.aggregate([{"$sample": {"size": 5}}]))
-                break
-        else:
-            # If no duplicate _id found, exit the loop
-            status = False
+        if st.button("🔁 Get Random Data 🔁"):
+            st.session_state["state"] = True
+            with st.status("Loading Data..."):
+                st.session_state["data"] = self.get_random_data()
 
-    return listdata
+        self.data_editor = StreamlitDataEditor(
+            data=st.session_state["data"] if st.session_state["state"] else None,
+            options=self.options,
+        )
 
+        if self.data_editor.is_data_available():
+            self.data_editor.generate_editor()
 
-def sendapi(data):
-    # st.info(f"Sending API... {data}")
-    # print(data)
-    # Insert data into emo_collection
-    with st.spinner("Wait for it..."):
-        emo_collection.insert_many(data)
+        if st.button("✉️ Send API ✉️"):
+            if st.session_state["state"]:
+                self.send_api(st.session_state["data"])
+            else:
+                st.warning("Please get random data first")
 
-    st.session_state["state"] = False
-    st.session_state["data"] = None
-    st.success("Send API Success")
-    st.components.v1.html(js)
-    # st.balloons(s)
+        if st.button("⬆️ Go Top ⬆️"):
+            js = """
+            <script>
+                var body = window.parent.document.querySelector(".main");
+                console.log(body);
+                body.scrollTop = 0;
+            </script>
+            """
+
+            st.components.v1.html(js)
 
 
 class StreamlitDataEditor:
@@ -82,10 +91,10 @@ class StreamlitDataEditor:
         self.options = options
 
     def generate_editor(self):
-        for i in range(len(self.data)):
-            st.write(f"keyword: {self.data[i]['context']}")
-            st.write(f"Generate: {self.data[i]['bot']}")
-            self.data[i]["Emotion"] = st.selectbox(
+        for i, item in enumerate(self.data):
+            st.write(f"keyword: {item['context']}")
+            st.write(f"Generate: {item['bot']}")
+            item["Emotion"] = st.selectbox(
                 f"Emotion for item {i+1}",
                 options=self.options,
                 key=f"emotion_{i}",
@@ -95,32 +104,8 @@ class StreamlitDataEditor:
         return self.data is not None
 
 
-# Usage
-if st.button("Get Random Data"):
-    st.session_state["state"] = True
-    with st.status("Loading Data..."):
-        st.session_state["data"] = getrandomdata()
-
-data_editor = StreamlitDataEditor(
-    data=st.session_state["data"] if st.session_state["state"] else None,
-    options=["Normal", "Luxury", "Chill", "Exclusive", "Bad"],
-)
-
-if data_editor.is_data_available():
-    data_editor.generate_editor()
-
-if st.button("Send API"):
-    if st.session_state["state"]:
-        sendapi(st.session_state["data"])
-    else:
-        st.warning("Please get random data first")
-
-# if st.button("Send API and Get Random Data"):
-#     if st.session_state["state"]:
-#         sendapi(st.session_state["data"])
-#         st.session_state["state"] = True
-#         with st.status("Loading Data..."):
-#             st.session_state["data"] = getrandomdata()
-#             print(st.session_state["data"])
-#     else:
-#         st.warning("Please get random data first")
+if __name__ == "__main__":
+    app = KeyToAdApp(
+        "mongodb+srv://64015037:2YqYA4kjsTImOMmY@keytoad.nslb9f9.mongodb.net/?retryWrites=true&w=majority"
+    )
+    app.run()
